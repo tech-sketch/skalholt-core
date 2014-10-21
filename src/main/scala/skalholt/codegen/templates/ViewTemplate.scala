f6515601c2d1b072d8818a1e9f75adf2ca03ba5a
yugolf <yugo.maede@gmail.com>
2014-10-21 16:02:47 +0900

    Delete table-bordered

--- src/main/scala/skalholt/codegen/templates/ViewTemplate.scala
+++ src/main/scala/skalholt/codegen/templates/ViewTemplate.scala
@@ -57,7 +57,7 @@ object ViewTemplate {
       <fieldset>
         <legend>results</legend>
         <div class="table-responsive">
-        <table class="table table-striped table-bordered table-hover table-condensed">
+        <table class="table table-striped table-hover table-condensed">
           <thead>
             <tr>
               ${
